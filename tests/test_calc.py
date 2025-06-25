b71b88f47f8adeefa93e5601f91ce9446b69be6e
1F10240124 <s1f102401247@iniad.org>
2025-06-25 17:10:15 +0900

    テストを追加

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -12,3 +12,22 @@ class TestTarget(unittest.TestCase):
     def test_fact_negative(self):
         with self.assertRaises(ValueError):
             fact(-1)
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_gcd_positive(self):
+        self.assertEqual(fact(7, 3), 1)
+        self.assertEqual(fact(56, 98), 14)
+
+    def test_gcd_negative(self):
+        self.assertEqual(fact(-15, -25), 5)
+        self.assertEqual(fact(-24, 32), 8)
+
+    def test_gcd_zero(self):
+        self.assertEqual(fact(5, 0), 5)
+        self.assertEqual(fact(0, 7), 7)
+        self.assertEqual(fact(0, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
