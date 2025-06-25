9ff39113254b80f5248011d7e4ef74c829a80e5d
1F10240124 <s1f102401247@iniad.org>
2025-06-25 17:05:25 +0900

    関数追加

--- project/calc.py
+++ project/calc.py
@@ -5,3 +5,18 @@ def fact(n):
     for i in range(1, n + 1):
         result *= i
     return result
+
+def gcd(a, b):
+ 
+    a, b = abs(a), abs(b)
+    
+    if a == 0 and b == 0:
+        return 0
+    elif a == 0:
+        return b
+    elif b == 0:
+        return a
+    
+    while b != 0:
+        a, b = b, a % b
+    return a
