ca9270b9896877001d84f80ea9f8de2f90b1bcec
1F10240124 <s1f102401247@iniad.org>
2025-06-25 17:19:33 +0900

    テストを追加

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,33 +1,33 @@
 import unittest
-from project.calc import fact
+from project.calc import gcd
 
 class TestTarget(unittest.TestCase):
     def test_fact_positive(self):
-        self.assertEqual(fact(1), 1)
-        self.assertEqual(fact(5), 120)
+        self.assertEqual(gcd(1), 1)
+        self.assertEqual(gcd(5), 120)
 
     def test_fact_zero(self):
-        self.assertEqual(fact(0), 1)
+        self.assertEqual(gcd(0), 1)
 
     def test_fact_negative(self):
         with self.assertRaises(ValueError):
-            fact(-1)
+            gcd(-1)
 
 
 class TestCalc(unittest.TestCase):
 
     def test_gcd_positive(self):
-        self.assertEqual(fact(7, 3), 1)
-        self.assertEqual(fact(56, 98), 14)
+        self.assertEqual(gcd(7, 3), 1)
+        self.assertEqual(gcd(56, 98), 14)
 
     def test_gcd_negative(self):
-        self.assertEqual(fact(-15, -25), 5)
-        self.assertEqual(fact(-24, 32), 8)
+        self.assertEqual(gcd(-15, -25), 5)
+        self.assertEqual(gcd(-24, 32), 8)
 
     def test_gcd_zero(self):
-        self.assertEqual(fact(5, 0), 5)
-        self.assertEqual(fact(0, 7), 7)
-        self.assertEqual(fact(0, 0), 0)
+        self.assertEqual(gcd(5, 0), 5)
+        self.assertEqual(gcd(0, 7), 7)
+        self.assertEqual(gcd(0, 0), 0)
 
 if __name__ == "__main__":
     unittest.main()
